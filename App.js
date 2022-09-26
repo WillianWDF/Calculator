f213036741f3fef929d62c448ad27baddf62ea4d
WillianWDF <williandamazio794@outlook.com>
2022-09-25 21:35:01 -0300

    Alteração de cores

--- App.js
+++ App.js
@@ -85,12 +85,14 @@ export default function App() {
 
         {buttons.map((button) => 
           button === '=' ? // Mapeamento do botão =
-            <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, { backgroundColor: '#3dd0e3' }]}>
+            <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, { backgroundColor: '#4d257a' }]}>
               <Text style={[styles.textButton, { color: "white", fontSize: 30 }]}>{button}</Text>
             </TouchableOpacity>
+
             : // Mapeamento dos outros botões
+            
             <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-              <Text style={[styles.textButton, { color: typeof (button) === 'number' ? 'black' : '#0093a6' }]}>{button}</Text>
+              <Text style={[styles.textButton, { color: typeof (button) === 'number' ? 'white' : '#b28cbf' }]}>{button}</Text>
             </TouchableOpacity>
         )}
       </View>
@@ -106,17 +108,17 @@ const styles = StyleSheet.create({
   results: {
     flex: 2,
     justifyContent: "center",
-    backgroundColor: "#f5f5f5"
+    backgroundColor: "#4d257a"
   },
   resultText: {
-    color: "#282F38",
+    color: "white",
     fontSize: 32,
     fontWeight: "bold",
     padding: 12,
     textAlign: "right"
   },
   historyText: {
-    color: "#7c7c7c",
+    color: "#cf9fc7",
     fontSize: 20,
     marginRight: 10,
     alignSelf: 'flex-end',
@@ -126,7 +128,7 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: '#885ea8',
     alignItems: 'center',
     justifyContent: 'center',
     minWidth: 90,
